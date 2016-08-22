c433fe2ae359761aa93b58b5b92e28ce4e4e5bb6
ww421 <sakasaen@gmail.com>
2016-08-22 23:54:15 +0200

    Commit RatesController with getDailyRates handler

---
+++ src/main/java/forex/rates/api/controller/RatesController.java
@@ -0,0 +1,80 @@
+package forex.rates.api.controller;
+
+import forex.rates.api.model.DailyRatesResponse;
+import forex.rates.api.model.ErrorResponse;
+import forex.rates.api.model.ExchangeRates;
+import forex.rates.api.service.AvailableCurrenciesService;
+import forex.rates.api.service.ExchangeRatesService;
+import forex.rates.api.service.DateTimeProviderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+@RestController
+@RequestMapping("rates")
+public class RatesController {
+
+    private AvailableCurrenciesService availableCurrenciesService;
+    private DateTimeProviderService dateTimeProviderService;
+    private ExchangeRatesService exchangeRatesService;
+
+    public RatesController(DateTimeProviderService dateTimeProviderService, AvailableCurrenciesService availableCurrenciesService, ExchangeRatesService exchangeRatesService) {
+	this.dateTimeProviderService = dateTimeProviderService;
+	this.availableCurrenciesService = availableCurrenciesService;
+	this.exchangeRatesService = exchangeRatesService;
+    }
+
+    @GetMapping("daily")
+    public DailyRatesResponse getDailyRates(
+	    @RequestParam(required = false, defaultValue = "USD") String base,
+	    @RequestParam(required = false) List<String> currencies,
+	    @RequestParam(required = false) String date
+    ) throws Exception {
+
+	if (!availableCurrenciesService.getList().contains(base)) {
+	    throw new IllegalArgumentException("The base you requested is invalid: " + base);
+	}
+
+	if (currencies != null) {
+	    for (String currency : currencies) {
+		if (!availableCurrenciesService.getList().contains(currency)) {
+		    throw new IllegalArgumentException("The currency you requested is invalid: " + currency);
+		}
+	    }
+	} else {
+	    currencies = availableCurrenciesService.getList();
+	}
+
+	if (date != null) {
+	    try {
+		LocalDate.parse(date);
+	    } catch (DateTimeParseException e) {
+		throw new IllegalArgumentException("The date you requested is invalid: " + date);
+	    }
+	} else {
+	    date = dateTimeProviderService.getTodaysDateAsString();
+	}
+
+	if (LocalDate.parse(date).isAfter(dateTimeProviderService.getTodaysDate())) {
+	    throw new IllegalArgumentException("The date you requested is out of range: " + date);
+	}
+
+	ExchangeRates exchangeRates = exchangeRatesService.getExchangeRatesFor(base, currencies, date);
+
+	if (exchangeRates.isEmpty()) {
+	    throw new IllegalArgumentException("Rates for the requested date are not available: " + exchangeRates.getDate());
+	}
+
+	return new DailyRatesResponse(dateTimeProviderService.getCurrentTimestamp(), exchangeRates);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResponse handleIllegalArgumentException(Exception e) {
+	return new ErrorResponse(dateTimeProviderService.getCurrentTimestamp(), HttpStatus.BAD_REQUEST,	e);
+    }
+
+}
