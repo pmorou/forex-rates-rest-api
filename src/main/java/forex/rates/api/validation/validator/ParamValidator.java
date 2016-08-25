94c2278f432e9dc186ca8568a200f351f71f52d3
ww421 <sakasaen@gmail.com>
2016-08-25 23:40:09 +0200

    Commit ParamValidator interface

---
+++ src/main/java/forex/rates/api/validation/validator/ParamValidator.java
@@ -0,0 +1,33 @@
+package forex.rates.api.validation.validator;
+
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+
+public interface ParamValidator<T> {
+
+    /**
+     * @param message message seen when validation fails
+     */
+    void setMessage(String message);
+
+    /**
+     * @param parameterType the {@link Class} of an instance this validation
+     * is being asked to validate
+     * @param annotations array of the {@link Annotation}s of an instance
+     * this validation is being asked to validate
+     * @return {@code true} if this {@link ParamValidator} implementation
+     * can validate instances of the supplied {@code parameterType}
+     * annotated with given {@code annotations}
+     */
+    boolean supports(Class<?> parameterType, Annotation... annotations);
+
+    /**
+     * Validate the supplied {@code object} instance, for which the
+     * {@link #supports(Class, Annotation...)} method return {@code true}.
+     * @param object the object that is to be validated
+     * @return validated object
+     * @throws IllegalArgumentException when validation fails
+     */
+    T validate(Optional<T> object);
+
+}
