9328ed73e8fe7e832dd7baa9733b98e69e31bf53
ww421 <sakasaen@gmail.com>
2016-08-26 22:58:16 +0200

    Apply ParamValidator's annotations on parameters

--- src/main/java/forex/rates/api/controller/RatesController.java
+++ src/main/java/forex/rates/api/controller/RatesController.java
@@ -3,66 +3,37 @@ package forex.rates.api.controller;
 import forex.rates.api.model.DailyRatesResponse;
 import forex.rates.api.model.ErrorResponse;
 import forex.rates.api.model.ExchangeRates;
-import forex.rates.api.service.AvailableCurrenciesService;
-import forex.rates.api.service.ExchangeRatesService;
 import forex.rates.api.service.DateTimeProviderService;
+import forex.rates.api.service.ExchangeRatesService;
+import forex.rates.api.validation.annotation.Base;
+import forex.rates.api.validation.annotation.Currencies;
+import forex.rates.api.validation.annotation.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @RestController
 @RequestMapping("rates")
 public class RatesController {
 
-    private AvailableCurrenciesService availableCurrenciesService;
     private DateTimeProviderService dateTimeProviderService;
     private ExchangeRatesService exchangeRatesService;
 
-    public RatesController(DateTimeProviderService dateTimeProviderService, AvailableCurrenciesService availableCurrenciesService, ExchangeRatesService exchangeRatesService) {
+    public RatesController(DateTimeProviderService dateTimeProviderService, ExchangeRatesService exchangeRatesService) {
 	this.dateTimeProviderService = dateTimeProviderService;
-	this.availableCurrenciesService = availableCurrenciesService;
 	this.exchangeRatesService = exchangeRatesService;
     }
 
     @GetMapping("daily")
     public DailyRatesResponse getDailyRates(
-	    List<String> currencies,
-	    String base,
-	    String date
+	    @Currencies List<String> currencies,
+	    @Base String base,
+	    @Date String date
     ) throws Exception {
 
-	if (!availableCurrenciesService.getList().contains(base)) {
-	    throw new IllegalArgumentException("The base you requested is invalid: " + base);
-	}
-
-	if (currencies != null) {
-	    for (String currency : currencies) {
-		if (!availableCurrenciesService.getList().contains(currency)) {
-		    throw new IllegalArgumentException("The currency you requested is invalid: " + currency);
-		}
-	    }
-	} else {
-	    currencies = availableCurrenciesService.getList();
-	}
-
-	LocalDate parsedDate = null;
-	if (date != null) {
-	    try {
-		parsedDate = LocalDate.parse(date);
-	    } catch (DateTimeParseException e) {
-		throw new IllegalArgumentException("The date you requested is invalid: " + date);
-	    }
-	} else {
-	    parsedDate = dateTimeProviderService.getTodaysDate();
-	}
-
-	if (parsedDate.isAfter(dateTimeProviderService.getTodaysDate())) {
-	    throw new IllegalArgumentException("The date you requested is out of range: " + parsedDate);
-	}
-
+	LocalDate parsedDate = LocalDate.parse(date);
 	ExchangeRates exchangeRates = exchangeRatesService.getExchangeRatesFor(base, currencies, parsedDate);
 
 	if (exchangeRates.isEmpty()) {
---
+++ src/test/java/forex/rates/api/controller/RatesControllerTest.java
@@ -0,0 +1,69 @@
+package forex.rates.api.controller;
+
+import forex.rates.api.model.ExchangeRates;
+import forex.rates.api.service.DateTimeProviderService;
+import forex.rates.api.service.ExchangeRatesService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class RatesControllerTest {
+    private @Mock DateTimeProviderService dateTimeProviderService;
+    private @Mock ExchangeRatesService exchangeRatesService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void before() {
+	MockitoAnnotations.initMocks(this);
+	RatesController ratesController = new RatesController(dateTimeProviderService, exchangeRatesService);
+	mockMvc = MockMvcBuilders.standaloneSetup(ratesController).build();
+    }
+
+    @Test
+    public void test_getDailyRates_returnValues() throws Exception {
+	List<String> currencies = Arrays.asList("EUR","PLN");
+	ExchangeRates result = createExchangeRates("BASE", "DATE", "RATE1", "RATE2");
+
+	when(dateTimeProviderService.getCurrentTimestamp()).thenReturn(1234L);
+	when(exchangeRatesService.getExchangeRatesFor(eq("USD"), eq(currencies), any(LocalDate.class)))
+		.thenReturn(result);
+
+	mockMvc.perform(get("/rates/daily?base=USD&date=2001-01-01&currencies=EUR,PLN")
+			.accept(MediaType.APPLICATION_JSON_VALUE))
+		.andExpect(status().isOk())
+		.andExpect(content().json("{'timestamp':1234}"))
+		.andExpect(content().json("{'date':DATE}"))
+		.andExpect(content().json("{'base':'BASE'}"))
+		.andExpect(content().json("{'rates':{'RATE1':1.0001,'RATE2':1.0001}}"));
+    }
+
+    private ExchangeRates createExchangeRates(String base, String date, String... currencies) {
+	ExchangeRates exchangeRates = new ExchangeRates();
+	Map<String, BigDecimal> rates = Arrays.stream(currencies)
+		.collect(toMap(e -> e, e -> new BigDecimal("1.0001")));
+	exchangeRates.setRates(rates);
+	exchangeRates.setBase(base);
+	exchangeRates.setDate(date);
+	return exchangeRates;
+    }
+
+}
