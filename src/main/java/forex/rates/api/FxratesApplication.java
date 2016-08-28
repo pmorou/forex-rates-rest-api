59612be1b46e8d1b2d9d1786276457cde102d0e1
ww421 <sakasaen@gmail.com>
2016-08-29 00:55:38 +0200

    Add and import swagger configuration

--- src/main/java/forex/rates/api/FxratesApplication.java
+++ src/main/java/forex/rates/api/FxratesApplication.java
@@ -2,8 +2,10 @@ package forex.rates.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class FxratesApplication {
 
 	public static void main(String[] args) {
