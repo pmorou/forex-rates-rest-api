ebda91f04fa61b40e0c3d41f4c892d48bb9aa65f
ww421 <sakasaen@gmail.com>
2016-08-28 23:21:29 +0200

    Add notes about connection between validator and annotation

--- src/main/java/forex/rates/api/validation/annotation/Base.java
+++ src/main/java/forex/rates/api/validation/annotation/Base.java
@@ -1,7 +1,12 @@
 package forex.rates.api.validation.annotation;
 
+import forex.rates.api.validation.validator.BaseParamValidator;
+
 import java.lang.annotation.*;
 
+/**
+ * Declares that a parameter should be validated by {@link BaseParamValidator}.
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- src/main/java/forex/rates/api/validation/annotation/Currencies.java
+++ src/main/java/forex/rates/api/validation/annotation/Currencies.java
@@ -1,7 +1,12 @@
 package forex.rates.api.validation.annotation;
 
+import forex.rates.api.validation.validator.CurrenciesParamValidator;
+
 import java.lang.annotation.*;
 
+/**
+ * Declares that a parameter should be validated by {@link CurrenciesParamValidator}.
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
