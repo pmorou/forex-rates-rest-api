8dae164f088770be74bbfd5b5cd01579d2f64d5b
ww421 <sakasaen@gmail.com>
2016-09-04 23:49:34 +0200

    Add JPA annotations

--- src/main/java/forex/rates/api/model/entity/CurrencyDefinition.java
+++ src/main/java/forex/rates/api/model/entity/CurrencyDefinition.java
@@ -2,11 +2,19 @@ package forex.rates.api.model.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "currencies")
 public class CurrencyDefinition {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String codeName;
+
     private int precision;
 
 }
--- src/main/java/forex/rates/api/model/entity/CurrencyRate.java
+++ src/main/java/forex/rates/api/model/entity/CurrencyRate.java
@@ -2,15 +2,25 @@ package forex.rates.api.model.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "rates")
 public class CurrencyRate {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private CurrencyDefinition currency;
+
     private BigDecimal exchangeRate;
+
     private LocalDate date;
+
+    @OneToOne
+    @JoinColumn(name = "currencies_id")
+    private CurrencyDefinition currency;
 
 }
