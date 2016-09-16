b1283b176d69f84121c84339821d5d8c12bd9ddb
ww421 <sakasaen@gmail.com>
2016-09-16 22:31:04 +0200

    Commit CurrencyRatesRepository interface

---
+++ src/main/java/forex/rates/api/repository/CurrencyRatesRepository.java
@@ -0,0 +1,14 @@
+package forex.rates.api.repository;
+
+import forex.rates.api.model.entity.CurrencyRate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CurrencyRatesRepository {
+
+    List<CurrencyRate> findAllByDateAndCurrenciesIn(List<String> currencies, LocalDate date);
+
+    CurrencyRate findOneByDateAndCurrenciesIn(String currency, LocalDate startDate);
+
+}
