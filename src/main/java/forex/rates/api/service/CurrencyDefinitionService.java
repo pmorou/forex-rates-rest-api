f86c909a92e9bf1a8ae05f74037b1524153e1a60
ww421 <sakasaen@gmail.com>
2016-09-23 01:54:52 +0200

    Add save method to CurrencyDefinitionService

--- src/main/java/forex/rates/api/service/CurrencyDefinitionService.java
+++ src/main/java/forex/rates/api/service/CurrencyDefinitionService.java
@@ -1,4 +1,9 @@
 package forex.rates.api.service;
 
+import forex.rates.api.model.entity.CurrencyDefinition;
+
 public interface CurrencyDefinitionService {
+
+    void save(CurrencyDefinition currencyDefinition);
+
 }
