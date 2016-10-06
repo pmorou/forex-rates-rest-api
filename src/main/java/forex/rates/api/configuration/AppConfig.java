a8d55b4d37ac61540e3c718cf6b9c651b65e4ca2
ww421 <sakasaen@gmail.com>
2016-10-07 01:43:32 +0200

    Add ThreadPoolTaskScheduler bean

--- src/main/java/forex/rates/api/configuration/AppConfig.java
+++ src/main/java/forex/rates/api/configuration/AppConfig.java
@@ -1,9 +1,18 @@
 package forex.rates.api.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @Import(SwaggerConfig.class)
 public class AppConfig {
+
+    @Bean
+    public TaskScheduler threadPoolTaskScheduler() {
+	return new ThreadPoolTaskScheduler();
+    }
+
 }
