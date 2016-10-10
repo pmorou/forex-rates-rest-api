686d94f392d180c6b82fc4641c8efef3aabe5df4
ww421 <sakasaen@gmail.com>
2016-10-10 23:58:13 +0200

    Implement DataSetUpdate for ECB

---
+++ src/main/java/forex/rates/api/schedule/DataSetUpdateEcbImpl.java
@@ -0,0 +1,104 @@
+package forex.rates.api.schedule;
+
+import forex.rates.api.autostart.SourceSettings;
+import forex.rates.api.autostart.dataset.ExtractedCurrencyRate;
+import forex.rates.api.http.client.HttpClient;
+import forex.rates.api.model.entity.CurrencyDefinition;
+import forex.rates.api.model.entity.CurrencyRate;
+import forex.rates.api.repository.CurrencyDefinitionRepository;
+import forex.rates.api.service.DateTimeProviderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.*;
+
+@Component
+@Slf4j
+public class DataSetUpdateEcbImpl implements DataSetUpdate {
+
+    private final HttpClient httpClient;
+    private final SourceSettings sourceSettings;
+    private final ExtractedCurrencyRate extractedCurrencyRate;
+    private final CurrencyDefinitionRepository currencyDefinitionRepository;
+    private final DateTimeProviderService dateTimeProviderService;
+
+    public DataSetUpdateEcbImpl(HttpClient httpClient, SourceSettings sourceSettings, ExtractedCurrencyRate extractedCurrencyRate,
+				CurrencyDefinitionRepository currencyDefinitionRepository, DateTimeProviderService dateTimeProviderService) {
+	this.httpClient = httpClient;
+	this.sourceSettings = sourceSettings;
+	this.extractedCurrencyRate = extractedCurrencyRate;
+	this.currencyDefinitionRepository = currencyDefinitionRepository;
+	this.dateTimeProviderService = dateTimeProviderService;
+    }
+
+    @Override
+    public List<CurrencyRate> getNewCurrencyRates() {
+	List<CurrencyRate> currencyRates = new ArrayList<>();
+	LocalDate ratesDate = dateTimeProviderService.getTodaysDate();
+
+	try {
+	    SAXParserFactory factory = SAXParserFactory.newInstance();
+	    SAXParser saxParser = factory.newSAXParser();
+
+	    DefaultHandler handler = new DefaultHandler() {
+		private static final String CUBE_TAG = "Cube";
+
+		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		    if (isInsideCubeTag(qName)) {
+			Map<String, String> attributesMap = mapAttributes(attributes);
+			String currency = attributesMap.get("currency");
+			if (currency != null) {
+			    CurrencyDefinition currencyDefinition = currencyDefinitionRepository.findOneByCodeName(currency);
+			    Map.Entry<String, String> entry = createEntry(attributesMap, ratesDate);
+			    CurrencyRate currencyRate = extractedCurrencyRate.getCurrencyRate(currencyDefinition, entry);
+			    currencyRates.add(currencyRate);
+			}
+		    }
+		}
+
+		private boolean isInsideCubeTag(String qName) {
+		    return CUBE_TAG.equalsIgnoreCase(qName);
+		}
+
+	    };
+
+	    try (InputStream inputStream = getInputStream()) {
+		saxParser.parse(inputStream, handler);
+	    } catch (IOException e) {
+		log.warn("Failed to get input stream", e);
+	    }
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+
+	return currencyRates;
+    }
+
+    private Map.Entry<String, String> createEntry(Map<String, String> attributesMap, LocalDate ratesDate) {
+	Map<String, String> rate = Collections.singletonMap(ratesDate.toString(), attributesMap.get("rate"));
+	return rate.entrySet().iterator().next();
+    }
+
+    private Map<String, String> mapAttributes(Attributes attributes) {
+	int numberOfAttributes = attributes.getLength();
+	Map<String, String> attributesMap = new HashMap<>(numberOfAttributes);
+	for (int i = 0; i < numberOfAttributes; i++) {
+	    attributesMap.put(attributes.getLocalName(i), attributes.getValue(i));
+	}
+	return attributesMap;
+    }
+
+    private InputStream getInputStream() throws IOException {
+	return httpClient.getInputStream(sourceSettings.getDailyDataSetUpdateUrl());
+    }
+
+}
---
+++ src/test/java/forex/rates/api/schedule/DataSetUpdateEcbImplTest.java
@@ -0,0 +1,96 @@
+package forex.rates.api.schedule;
+
+import forex.rates.api.autostart.SourceSettings;
+import forex.rates.api.autostart.dataset.ExtractedCurrencyRate;
+import forex.rates.api.http.client.HttpClient;
+import forex.rates.api.model.entity.CurrencyDefinition;
+import forex.rates.api.model.entity.CurrencyRate;
+import forex.rates.api.repository.CurrencyDefinitionRepository;
+import forex.rates.api.service.DateTimeProviderService;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+public class DataSetUpdateEcbImplTest {
+
+    private final CurrencyDefinition USD_DEFINITION = createCurrencyDefinition("USD", 4);
+    private final CurrencyDefinition JPY_DEFINITION = createCurrencyDefinition("JPY", 2);
+    private final Map.Entry<String, String> USD_ENTRY = createEntry("2001-01-01", "1.1185");
+    private final Map.Entry<String, String> JPY_ENTRY = createEntry("2001-01-01", "116.00");
+    private final CurrencyRate JPY_CURRENCY_RATE = createCurrencyRate(JPY_DEFINITION, JPY_ENTRY);
+    private final CurrencyRate USD_CURRENCY_RATE = createCurrencyRate(USD_DEFINITION, USD_ENTRY);
+
+    private @Mock HttpClient httpClient;
+    private @Mock SourceSettings sourceSettings;
+    private @Mock ExtractedCurrencyRate extractedCurrencyRate;
+    private @Mock CurrencyDefinitionRepository currencyDefinitionRepository;
+    private @Mock DateTimeProviderService dateTimeProviderService;
+
+    private DataSetUpdate dataSetUpdateEcb;
+
+    @Before
+    public void setUp() throws Exception {
+	MockitoAnnotations.initMocks(this);
+	dataSetUpdateEcb = new DataSetUpdateEcbImpl(httpClient, sourceSettings, extractedCurrencyRate,
+		currencyDefinitionRepository, dateTimeProviderService);
+    }
+
+    @Test
+    public void shouldReturnUsdAndJpyCurrencyRates() throws Exception {
+	// Given
+	when(httpClient.getInputStream(any())).thenReturn(getTestInputStream());
+	when(dateTimeProviderService.getTodaysDate()).thenReturn(LocalDate.of(2001,1,1));
+	when(currencyDefinitionRepository.findOneByCodeName("USD")).thenReturn(USD_DEFINITION);
+	when(currencyDefinitionRepository.findOneByCodeName("JPY")).thenReturn(JPY_DEFINITION);
+	when(extractedCurrencyRate.getCurrencyRate(USD_DEFINITION, USD_ENTRY)).thenReturn(USD_CURRENCY_RATE);
+	when(extractedCurrencyRate.getCurrencyRate(JPY_DEFINITION, JPY_ENTRY)).thenReturn(JPY_CURRENCY_RATE);
+
+	// When
+	List<CurrencyRate> currencyRates = dataSetUpdateEcb.getNewCurrencyRates();
+
+	// Then
+	assertThat(currencyRates).isNotNull();
+	assertThat(currencyRates).containsOnly(USD_CURRENCY_RATE, JPY_CURRENCY_RATE);
+    }
+
+    private InputStream getTestInputStream() throws IOException {
+	return FileUtils.openInputStream(new File("src/test/resources/testDataSetUpdate_ecb.xml"));
+    }
+
+    private Map.Entry<String, String> createEntry(String date, String rate) {
+	return Collections.singletonMap(date, rate).entrySet().iterator().next();
+    }
+
+    private CurrencyDefinition createCurrencyDefinition(String codeName, int precision) {
+	CurrencyDefinition currencyDefinition = new CurrencyDefinition();
+	currencyDefinition.setCodeName(codeName);
+	currencyDefinition.setPrecision(precision);
+	return currencyDefinition;
+    }
+
+    private CurrencyRate createCurrencyRate(CurrencyDefinition currencyDefinition, Map.Entry<String, String> entry) {
+	CurrencyRate currencyRate = new CurrencyRate();
+	String date = entry.getKey();
+	String rate = entry.getValue();
+	currencyRate.setExchangeRate(new BigDecimal(rate));
+	currencyRate.setCurrency(currencyDefinition);
+	currencyRate.setDate(LocalDate.parse(date));
+	return currencyRate;
+    }
+
+}
