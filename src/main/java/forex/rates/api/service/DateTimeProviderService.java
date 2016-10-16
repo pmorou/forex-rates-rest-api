cd2d91913c9bb74250a286d52f4def8e9cb5968d
ww421 <sakasaen@gmail.com>
2016-10-16 22:04:26 +0200

    Add javadoc for DateTimeProviderService's methods

--- src/main/java/forex/rates/api/service/DateTimeProviderService.java
+++ src/main/java/forex/rates/api/service/DateTimeProviderService.java
@@ -4,10 +4,19 @@ import java.time.LocalDate;
 
 public interface DateTimeProviderService {
 
+    /**
+     * @return UTC time in milliseconds
+     */
     long getCurrentTimestamp();
 
+    /**
+     * @return {@link #getTodaysDate()} as String
+     */
     String getTodaysDateAsString();
 
+    /**
+     * @return UTC time in YYYY-MM-DD format
+     */
     LocalDate getTodaysDate();
 
 }
