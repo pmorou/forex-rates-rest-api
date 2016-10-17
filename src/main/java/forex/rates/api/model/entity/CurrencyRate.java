0ba8ab8992411359996051d2c6510f8257f6bff2
ww421 <sakasaen@gmail.com>
2016-10-18 01:58:16 +0200

    Specify exchangeRate's column precision and scale

--- src/main/java/forex/rates/api/model/entity/CurrencyRate.java
+++ src/main/java/forex/rates/api/model/entity/CurrencyRate.java
@@ -17,6 +17,7 @@ public class CurrencyRate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(precision = 10, scale = 5)
     private BigDecimal exchangeRate;
 
     private LocalDate date;
