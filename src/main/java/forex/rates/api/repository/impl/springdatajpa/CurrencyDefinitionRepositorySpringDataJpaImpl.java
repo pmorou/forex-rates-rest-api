becb742125f497d391c91a0868a812bbcfd06fe8
ww421 <sakasaen@gmail.com>
2016-10-22 01:59:12 +0200

    Assign spring data jpa repositories to spring-data-jpa profile

--- src/main/java/forex/rates/api/repository/impl/springdatajpa/CurrencyDefinitionRepositorySpringDataJpaImpl.java
+++ src/main/java/forex/rates/api/repository/impl/springdatajpa/CurrencyDefinitionRepositorySpringDataJpaImpl.java
@@ -2,7 +2,9 @@ package forex.rates.api.repository.impl.springdatajpa;
 
 import forex.rates.api.model.entity.CurrencyDefinition;
 import forex.rates.api.repository.CurrencyDefinitionRepository;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.Repository;
 
+@Profile("spring-data-jpa")
 public interface CurrencyDefinitionRepositorySpringDataJpaImpl extends CurrencyDefinitionRepository, Repository<CurrencyDefinition, Long> {
 }
--- src/main/java/forex/rates/api/repository/impl/springdatajpa/CurrencyRateRepositorySpringDataJpaImpl.java
+++ src/main/java/forex/rates/api/repository/impl/springdatajpa/CurrencyRateRepositorySpringDataJpaImpl.java
@@ -2,7 +2,9 @@ package forex.rates.api.repository.impl.springdatajpa;
 
 import forex.rates.api.model.entity.CurrencyRate;
 import forex.rates.api.repository.CurrencyRateRepository;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.Repository;
 
+@Profile("spring-data-jpa")
 public interface CurrencyRateRepositorySpringDataJpaImpl extends CurrencyRateRepository, Repository<CurrencyRate, Long> {
 }
