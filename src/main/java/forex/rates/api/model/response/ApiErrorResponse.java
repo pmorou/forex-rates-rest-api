1a2cd7b9957faf3b60684950822765083a09363a
ww421 <sakasaen@gmail.com>
2016-10-21 02:56:29 +0200

    Return default message when none found

--- src/main/java/forex/rates/api/model/response/ApiErrorResponse.java
+++ src/main/java/forex/rates/api/model/response/ApiErrorResponse.java
@@ -1,12 +1,17 @@
 package forex.rates.api.model.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Data
 @JsonPropertyOrder({ "error", "httpStatus", "message", "description" })
 public class ApiErrorResponse {
+
+    @Getter(onMethod = @__(@JsonIgnore))
+    private final String DEFAULT_MESSAGE = "No message available";
 
     private final boolean error;
     private final int httpStatus;
@@ -16,8 +21,12 @@ public class ApiErrorResponse {
     public ApiErrorResponse(HttpStatus httpStatus, Exception exception) {
 	this.message = httpStatus.getReasonPhrase();
 	this.httpStatus = httpStatus.value();
-	this.description = exception.getMessage();
+	this.description = getMessage(exception);
 	this.error = true;
+    }
+
+    private String getMessage(Exception exception) {
+	return exception.getMessage() == null ? DEFAULT_MESSAGE : exception.getMessage();
     }
 
 }
