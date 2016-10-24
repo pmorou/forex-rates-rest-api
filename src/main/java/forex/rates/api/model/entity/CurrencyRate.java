f7e1cd5fb3724d9cd427a26b18cfa32017e48488
ww421 <sakasaen@gmail.com>
2016-10-24 20:01:18 +0200

    Increase precision and scale

--- src/main/java/forex/rates/api/model/entity/CurrencyRate.java
+++ src/main/java/forex/rates/api/model/entity/CurrencyRate.java
@@ -17,7 +17,7 @@ public class CurrencyRate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(precision = 10, scale = 5)
+    @Column(precision = 15, scale = 7)
     private BigDecimal exchangeRate;
 
     private LocalDate date;
