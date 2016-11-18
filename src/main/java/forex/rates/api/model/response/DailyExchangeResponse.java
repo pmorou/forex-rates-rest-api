6e2ad59d599a047652dd05f78d1e1a9fa471fabf
ww421 <sakasaen@gmail.com>
2016-11-18 01:55:39 +0100

    Commit DailyExchangeResponse

---
+++ src/main/java/forex/rates/api/model/response/DailyExchangeResponse.java
@@ -0,0 +1,26 @@
+package forex.rates.api.model.response;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import forex.rates.api.model.ExchangeTransaction;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@JsonPropertyOrder({ "date", "amount", "from", "to" })
+public class DailyExchangeResponse {
+
+    private final String date;
+    private final int amount;
+    private final String from;
+    private final Map<String, BigDecimal> to;
+
+    public DailyExchangeResponse(ExchangeTransaction exchangeTransaction) {
+	this.date = exchangeTransaction.getDate().toString();
+	this.amount = exchangeTransaction.getAmount();
+	this.from = exchangeTransaction.getFrom();
+	this.to = exchangeTransaction.getTo();
+    }
+
+}
